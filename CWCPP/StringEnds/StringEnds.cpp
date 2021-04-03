8a81ed28483d01de1e5dd1168ca6f68e048e5959
Ahmad Zafar <62062766+TheTurtle3@users.noreply.github.com>
2021-04-03 18:32:08 -0400

    Create StringEnds.cpp

---
+++ CWCPP/StringEnds/StringEnds.cpp
@@ -0,0 +1,15 @@
+//Ahmad Zafar
+//Find if the second string is the end of the first string
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string const &str, string const &ending) {
+    
+}
+
+int main() {
+
+}
