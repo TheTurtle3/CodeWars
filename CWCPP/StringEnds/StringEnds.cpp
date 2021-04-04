47ca0744b547a4fd494f09b4440ad12bcfbce8a3
Ahmad Zafar <62062766+TheTurtle3@users.noreply.github.com>
2021-04-03 21:40:08 -0400

    Update StringEnds.cpp

--- CWCPP/StringEnds/StringEnds.cpp
+++ CWCPP/StringEnds/StringEnds.cpp
@@ -3,13 +3,29 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 bool solution(string const &str, string const &ending) {
-    
+  if(str.length() < ending.length()) {
+    return false;
+  }
+  else if(str.substr(str.length() - ending.length()) == ending) {
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 int main() {
+    if(solution("abcd","") == true){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
 
+    return 0;
 }
