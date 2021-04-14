a4b470d8bbda97bc2e9a460a083a25d8dc06580b
Ahmad Zafar <62062766+TheTurtle3@users.noreply.github.com>
2021-04-13 21:12:22 -0400

    Create IsANumberPrime.cpp

---
+++ CWCPP/IsANumberPrime/IsANumberPrime.cpp
@@ -0,0 +1,41 @@
+// Ahmad Zafar
+// Is the entered number a prime number?
+
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+// Solution
+bool isPrime(int num) {
+  if(num <= 1) {
+    return false;
+  }
+  
+  if(num == 2) {
+    return true;
+  }
+  
+  for(int i = 2; i < sqrt(num); i++) {
+    if(num % i == 0) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+// User Input
+int main() {
+    int input;
+    
+    printf("Enter an integer: ");
+    scanf("%d", &input);
+
+    if(isPrime(input) == true){
+        printf("%d is a prime number!", input);
+    }
+    else{
+        printf("%d is not a prime number!", input);
+    }
+}
